e7d3b81fd0385468a120242d8b113bc51e3bf8a3
nazibot1 <42460638+nazibot1@users.noreply.github.com>
2018-08-17 15:09:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,6 @@ const getYoutubeID = require('get-youtube-id');
 
 const fetchVideoInfo = require('youtube-info');
 
-const YouTube = require('simple-youtube-api');
-
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 
 const queue = new Map();
